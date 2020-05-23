19d7d8cfd5e21ced6016e0487b8e481409634c8f
ruotingw <wruoting@gmail.com>
2020-05-23 12:06:22 -0400

    Faster compuations

--- assignment_1_wang_wmt_q1_q2.py
+++ assignment_1_wang_wmt_q1_q2.py
@@ -6,6 +6,7 @@ import os
 import numpy as np
 import datetime as dt
 from numpy import genfromtxt
+from collections import deque
 
 ticker='WMT'
 
@@ -36,46 +37,39 @@ def pnl_per_share(profit, shares):
 
 # Returns profit from close to open given a row
 def open_to_close_rows(row, strat='long', money=100):
-    # Subtract adj_close from open (not adj close)
+    # Depending on strat, process adj close to open
     adj_close = row[12].astype(np.float32)
     open_current_day = row[7].astype(np.float32)
     return open_to_close(open_current_day, adj_close, strat=strat, money=money)
 
 
 def trade_with_threshold(ticker_data, threshold=0):
-    trading_strategy_long = np.array([])
-    trading_strategy_long_per_share = np.array([])
-    trading_strategy_short = np.array([])
-    trading_strategy_short_per_share = np.array([])
-    trading_strategy_total = np.array([])
-    trading_strategy_total_per_share = np.array([])
+    # Dequeue faster than np array, we can convert back at the end
+    trading_strategy_long = deque()
+    trading_strategy_short = deque()
+    trading_strategy_total = deque()
     for index, row in enumerate(ticker_data):
-        if (index > 1 and index != np.size(ticker_data, axis=0)-1):
+        if (index > 1 and index != np.size(ticker_data, axis=0)):
             # Trading Strategy Long
             if(process_adj_open(row, ticker_data[index-1]) > threshold):
                 profit, number_of_shares = open_to_close_rows(ticker_data[index], strat='long')
-                pnl_share = pnl_per_share(profit, number_of_shares)
-                trading_strategy_long = np.append(trading_strategy_long, profit)
-                trading_strategy_long_per_share = np.append(trading_strategy_long_per_share, pnl_share)
+                trading_strategy_long.append(profit)
                 # Append to total strategy profit
-                trading_strategy_total = np.append(trading_strategy_total, profit)
-                trading_strategy_total_per_share = np.append(trading_strategy_total_per_share, pnl_share)
+                trading_strategy_total.append(profit)
             # Trading Strategy Short
             if(process_adj_open(row, ticker_data[index-1]) < threshold):
                 profit, number_of_shares = open_to_close_rows(ticker_data[index], strat='short')
-                pnl_share = pnl_per_share(profit, number_of_shares)
-                trading_strategy_short = np.append(trading_strategy_short, profit)
-                trading_strategy_short_per_share = np.append(trading_strategy_short_per_share, pnl_share)
+                trading_strategy_short.append(profit)
                 # Append to total strategy profit
-                trading_strategy_total = np.append(trading_strategy_total, profit)
-                trading_strategy_total_per_share = np.append(trading_strategy_total_per_share, pnl_share)
-
-    return trading_strategy_long, trading_strategy_short, trading_strategy_total, trading_strategy_long_per_share, trading_strategy_short_per_share, trading_strategy_total_per_share
+                trading_strategy_total.append(profit)
+    trading_strategy_long = np.asarray(trading_strategy_long)
+    trading_strategy_short = np.asarray(trading_strategy_short)
+    trading_strategy_total = np.asarray(trading_strategy_total)
+    return trading_strategy_long, trading_strategy_short, trading_strategy_total
 
 
 def average_profit_per_trade(ticker_data, threshold=0):
-    trading_strategy_long, trading_strategy_short, trading_strategy_total, trading_strategy_long_per_share, trading_strategy_short_per_share, trading_strategy_total_per_share = \
-    trade_with_threshold(ticker_data, threshold=threshold)
+    trading_strategy_long, trading_strategy_short, trading_strategy_total = trade_with_threshold(ticker_data, threshold=threshold)
     sum_pnl = np.sum(trading_strategy_total)
     num_days =np.size(trading_strategy_total)
     return np.round(np.divide(sum_pnl, num_days), 2)
@@ -84,37 +78,27 @@ def main():
     try:
         # Create ticker data from csv without having to read lines  
         ticker_data = genfromtxt('./{}.csv'.format(ticker), delimiter=',', dtype=str)
-        trading_strategy_1, trading_strategy_2, total_trading_strategy, trading_strategy_1_per_share, trading_strategy_2_per_share, total_trading_strategy_per_share = \
+        trading_strategy_1, trading_strategy_2, total_trading_strategy = \
             trade_with_threshold(ticker_data)
         # Question 1
         # Sum all the pnls and divide by number of days
         sum_pnl = np.add(np.sum(trading_strategy_1), np.sum(trading_strategy_2))
-        sum_pnl_per_share = np.add(np.sum(trading_strategy_1_per_share), np.sum(trading_strategy_2_per_share))
         num_days = np.add(np.size(trading_strategy_1), np.size(trading_strategy_2))
         # Round to two decimal places
         average_pnl = np.round(np.divide(sum_pnl, num_days), 2)
-        average_pnl_per_share = np.round(np.divide(sum_pnl_per_share, num_days), 2)
 
         # Question 2
         sum_pnl_long = np.sum(trading_strategy_1)
         sum_pnl_short = np.sum(trading_strategy_2)
-        # Question 3
-        x_data = np.arange(0, 10, 0.1)
-        y_data = np.array([])
-        for threshold in x_data:
-            y_data = np.append(y_data, average_profit_per_trade(ticker_data, threshold=threshold))
-
 
         print('Daily Profit:')
         print('Longs: {}'.format(trading_strategy_1))
         print('Shorts: {}'.format(trading_strategy_2))
-        print('Longs per share: {}'.format(trading_strategy_1_per_share))
-        print('Shorts per share: {}'.format(trading_strategy_2_per_share))
         print('Question 1:')
-        print('Average daily profit: {}'.format(average_pnl))
+        print('Average total daily profit: ${}'.format(average_pnl))
         print('Question 2:')
-        print('Long position profit: {}'.format(sum_pnl_long))
-        print('Short position profit: {}'.format(sum_pnl_short))
+        print('Long position profit: ${}'.format(sum_pnl_long))
+        print('Short position profit: ${}'.format(sum_pnl_short))
         print('The short position profits are higher than the long position profits')
 
     except Exception as e:
--- assignment_1_wang_wmt_q3.py
+++ assignment_1_wang_wmt_q3.py
@@ -8,6 +8,7 @@ import datetime as dt
 from numpy import genfromtxt
 import matplotlib.pyplot as plt
 from assignment_1_wang_wmt_q1_q2 import average_profit_per_trade
+from collections import deque
 
 ticker='WMT'
 
@@ -17,10 +18,11 @@ def main():
         ticker_data = genfromtxt('./{}.csv'.format(ticker), delimiter=',', dtype=str)
         # Question 3
         x_data = np.arange(0, 10, 0.1)
-        y_data = np.array([])
+        y_data = deque()
         print('Plotting over 100 points for threshold')
         for threshold in x_data:
-            y_data = np.append(y_data, average_profit_per_trade(ticker_data, threshold=threshold))
+            y_data.append(average_profit_per_trade(ticker_data, threshold=threshold))
+        y_data = np.array(y_data)
         plt.plot(x_data, y_data, label='P/L vs. Threshold to trade')
         plt.xlabel('Threshold for Trading (%)')
         plt.ylabel('Profit ($)')
